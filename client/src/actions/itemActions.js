a2a4e7d15ad52fe845cb737e4cebaa145d6d4d80
deva40211 <devashish15262@gmail.com>
2023-12-30 15:50:34 +0530

    hi

--- client/src/actions/itemActions.js
+++ client/src/actions/itemActions.js
@@ -1,44 +1,46 @@
 import axios from 'axios';//http client. it sends request for get, post, etc to the defined address
-import {GET_ITEMS, ADD_ITEM, DELETE_ITEM, ITEMS_LOADING} from './types';
+import { GET_ITEMS, ADD_ITEM, DELETE_ITEM, ITEMS_LOADING } from './types';
 import { tokenConfig } from './authActions';
 import { returnErrors } from './errorActions';
 
-export const getItems=()=>dispatch=>{//dispatch allows use to make a request
+const serverAddress = `https://shopping-list-pwnh.onrender.com`;
+
+export const getItems = () => dispatch => {//dispatch allows use to make a request
     dispatch(setItemsLoading());
     axios
-    .get('/api/items')//makes the request to specified path in backend (from routes)
-    .then(res=>
-        dispatch({
-            type: GET_ITEMS,
-            payload: res.data//sends back payload to given reducer as data of respond received
-        }))
-    .catch(err=>dispatch(returnErrors(err.response.data, err.response.status)));
+        .get(`${serverAddress}/api/items`)//makes the request to specified path in backend (from routes)
+        .then(res =>
+            dispatch({
+                type: GET_ITEMS,
+                payload: res.data//sends back payload to given reducer as data of respond received
+            }))
+        .catch(err => dispatch(returnErrors(err.response.data, err.response.status)));
 };//sends the GET_ITEMS to ../reducer/itemReducer
 
 
-export const addItem=(item)=>(dispatch, getState)=>{
+export const addItem = (item) => (dispatch, getState) => {
     axios
-    .post('/api/items', item, tokenConfig(getState))//passes item (obtained from onSubmit from itemModal) to api/items in router
-    .then(res=>
-        dispatch({
-            type: ADD_ITEM,
-            payload: res.data//sends (to the reducer) the new item we added 
-        }))
-    .catch(err=>dispatch(returnErrors(err.response.data, err.response.status)));
+        .post(`${serverAddress}/api/items`, item, tokenConfig(getState))//passes item (obtained from onSubmit from itemModal) to api/items in router
+        .then(res =>
+            dispatch({
+                type: ADD_ITEM,
+                payload: res.data//sends (to the reducer) the new item we added 
+            }))
+        .catch(err => dispatch(returnErrors(err.response.data, err.response.status)));
 };
-export const deleteItem=(id)=>(dispatch, getState)=>{
+export const deleteItem = (id) => (dispatch, getState) => {
     axios
-    .delete(`/api/items/${id}`, tokenConfig(getState))//attaches the token in header
-    .then(res=>
-        dispatch({
-            type: DELETE_ITEM,
-            payload: id
-        })
-    )
-    .catch(err=>dispatch(returnErrors(err.response.data, err.response.status)));
+        .delete(`${serverAddress}/api/items/${id}`, tokenConfig(getState))//attaches the token in header
+        .then(res =>
+            dispatch({
+                type: DELETE_ITEM,
+                payload: id
+            })
+        )
+        .catch(err => dispatch(returnErrors(err.response.data, err.response.status)));
 };
-export const setItemsLoading=()=>{
-    return{
+export const setItemsLoading = () => {
+    return {
         type: ITEMS_LOADING
     };
 };
