b34bf53258262ac8558fddffce0ee6ae5864266b
deva40211 <devashish15262@gmail.com>
2023-12-30 15:08:12 +0530

    final commit

--- server.js
+++ server.js
@@ -1,25 +1,25 @@
-const express=require('express');
-const mongoose=require('mongoose');
-const config=require('config');
+const express = require('express');
+const mongoose = require('mongoose');
+const config = require('config');
 
-const app=express();
+const app = express();
 app.use(express.json());
 app.use('/api/items', require('./routes/api/items'));
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
-app.get('/', (req, res)=>{
+app.get('/', (req, res) => {
     res.write('Hello\nFinally');
     res.end();
 });
 
-const db=config.get('mongoURI');
+const db = config.get('mongoURI');
 mongoose
-.connect(db, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
-.then(()=> console.log('mongoDB connected'))
-.catch(err=> console.log(`An error has occurred\n${err}`));
+    .connect(db, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => console.log('mongoDB connected'))
+    .catch(err => console.log(`An error has occurred\n${err}`));
 
-const port=process.env.PORT || 5000;
-app.listen(port, ()=> console.log(`listening on ${port}`));
+const port = 5000;
+app.listen(port, () => console.log(`listening on ${port}`));
